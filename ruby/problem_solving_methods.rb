cd79c17ad916487aa7d5bb19a43fae99325f1fa6
Rafael Lara <ing.lara@live.com>
2017-02-03 10:53:36 -0600

    Calculate Fibonacci Numbers

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -22,3 +22,22 @@ arr = [42, 89, 23, 1]
 
 p search_array(arr, 1)
 p search_array(arr, 24)
+
+def fib(number)
+	array = [0,1]
+	case number
+	when 1
+		return array = [0]
+	when 2
+		return array
+	end
+	while number > 2 do
+		array << array[-2] + array[-1]
+		number-= 1
+	end
+	array
+end
+
+p fib(1)
+p fib(6)
+p fib(100)[-1] == 218922995834555169026
