4651f14ac8a62f2343ad210304677818c3e0cae3
Rafael Lara <ing.lara@live.com>
2017-02-06 14:21:26 -0600

    Add santa class and test code

---
+++ ruby/santa.rb
@@ -0,0 +1,17 @@
+class Santa
+	def initialize
+		p "Initializing Santa instance ..."
+	end
+
+	def speak 
+		p "Ho, ho, ho! Haaaappy holidays!"
+	end
+
+	def eat_milk_and_cookies(cookie_type)
+		p "That was a good #{cookie_type}!" 
+	end
+	
+end
+santa1 = Santa.new
+santa1.speak
+santa1.eat_milk_and_cookies("oreo")
