384720d20e9c97e68f5f5e29f34a178769495c02
Rafael Lara <ing.lara@live.com>
2017-02-08 16:03:05 -0600

    6.5 pairing exercises

---
+++ ruby/calculator_reviewed.rb
@@ -0,0 +1,54 @@
+#integers operations available +, -, *,/
+#input:"2 + 3"
+#output:["2","+","3"]
+def divide_by_space(input)
+  input = string.split(" ")
+end
+
+p divide_by_space("2 + 3")
+
+
+
+# def calculate(num1,operation,num2)
+#   case operation
+#   when "+"
+#     answer = num1+num2
+#   when "-"
+#     answer = num1 - num2
+#   when "*"
+#     answer = num1 * num2
+#   when "/"
+#     answer = num1/num2  
+#   end
+# end 
+
+# # #DRIVER CODE
+# # p calculate(5, '+', 7)# =>  12
+# # p calculate(10,'-',5)# => 5
+# # p calculate(2,'*',5) # =>  10
+# # p calculate(20,'/',5)# => 4 
+
+# #USER INTERFACE
+puts "Enter your "
+ 
+
+
+
+# more_calculations = true
+# ops =[]
+# calculations = 0 
+# while more_calculations do 
+#   puts "Type your operation (+,-,*,/). Remember its limited for just one operation at a time."
+#   operation = gets.chomp
+#   ops << operation 
+#   if operation == "done"
+#     more_calculations = false
+#     puts "#{calculations} calculations performed:"
+#     puts ops
+#   else
+#     calculations += 1
+#     puts "your answer:"
+#     p answer = calculate(operation)
+#     ops << answer
+#   end
+# end
