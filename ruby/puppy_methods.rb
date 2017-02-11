9192e0b2f80eb2f3056203f03712d46a41b9e4c0
Rafael Lara <ing.lara@live.com>
2017-02-11 14:15:12 -0600

    Add speak and soll over method

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,23 @@
 class Puppy
-
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
   end
 
+  def speak(number)
+  	number.times { puts "Woof!"}
+  end
+
+  def roll_over
+  	puts "*rolls over*"
+  end
 end
 
+#DRIVER CODE 
+puppy = Puppy.new
+puppy.speak(5)
+puts "---------"
+puppy.speak(3)
+puts "---------"
+puppy.roll_over
+puts "---------"
