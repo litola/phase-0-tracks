a83afb178ae4b9964b7fbfeb540fd6f065f698a3
Rafael Lara <ing.lara@live.com>
2017-02-11 14:40:03 -0600

    Release 2: Write your own class, and experiment

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,4 +1,8 @@
 class Puppy
+	def initialize 
+		puts "Initializing new puppy instance ..."
+	end
+
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
@@ -23,6 +27,7 @@ end
 
 #DRIVER CODE 
 puppy = Puppy.new
+puts "---------"
 puppy.speak(5)
 puts "---------"
 puppy.speak(3)
@@ -32,3 +37,29 @@ puts "---------"
 puts puppy.dog_years(7)
 puts "---------"
 puppy.play_death
+
+class Parrot
+	def initialize 
+		puts "Parrot talking: Parrot initilaizing .. grrrr grrr"
+	end
+
+	def talk 
+		puts "GGrrr, Grrr Parrot can talk!"
+	end
+
+	def fly(destination)
+		puts "Parrot flying to #{destination}!"
+	end
+end
+
+#Driver code
+parrots = {}
+50.times { |x| parrots.store("parrot_#{x}",Parrot.new) }
+
+puts parrots
+parrots.each do |name,parrot|
+	puts "Name: #{name}"
+	parrot.talk
+	parrot.fly("Europe")
+end
+
