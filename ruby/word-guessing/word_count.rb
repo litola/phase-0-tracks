3f891d89a7ee3ac2e1f6fe0c6a59433fa7370084
Rafael Lara <ing.lara@live.com>
2017-02-11 15:23:09 -0600

    Add dir and pseudo code

---
+++ ruby/word-guessing/word_count.rb
@@ -0,0 +1,11 @@
+#PSEUDO CODE 
+#User 1: Enters word or phrase to be guess
+# => save word or phrase
+# => change word or phrase to ex.    word to _ _ _ _
+# => limit the number of guesses to letters in phrase or word
+
+#User 2: Make N guesses to win or lose
+# =>  Display the initial word or phrase in blank spaces to be guessed word = _ _ _ _
+# => If letter in phrase display W _ _ _ , N guesses left!
+# => If win "Congratulations! You won!" message 
+# => else "Better luck next time!"
