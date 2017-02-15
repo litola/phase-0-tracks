5c7e1d8f07e1d82ac61e1671c2219441ecef56d5
Rafael Lara <ing.lara@live.com>
2017-02-14 23:09:29 -0600

    Modified the => in hashes and removed :

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,23 +1,23 @@
 refrigerator={
-	"top shelf": ["milk", "cheese"],
-	"middle shelf": ["orange juice", "salsa", "yougurt"]  ,
-	"bottom shel": ["water", "coca cola", "beer"],
-	"drawers": {
-		"top": ["oranges", "banannas"],
-		"bottom": ["lettuce", "tomato", "onions"]
+	"top shelf" => ["milk", "cheese"],
+	"middle shelf" => ["orange juice", "salsa", "yougurt"]  ,
+	"bottom shel" =>["water", "coca cola", "beer"],
+	"drawers" => {
+		"top" => ["oranges", "banannas"],
+		"bottom" => ["lettuce", "tomato", "onions"]
 	}
 }
 
 #milk
-p refrigerator[:"top shelf"][0]
+p refrigerator["top shelf"][0]
 
 #banannas
-p refrigerator[:"drawers"][:"top"][1]
+p refrigerator["drawers"]["top"][1]
 
 #list bottom drawer
-p refrigerator[:"drawers"][:"bottom"]
+p refrigerator["drawers"]["bottom"]
 
 #change oranges to carrots
-p refrigerator[:"drawers"][:"top"]
-refrigerator[:"drawers"][:"top"][0] = "carrots"
-p refrigerator[:"drawers"][:"top"]
+p refrigerator["drawers"]["top"]
+refrigerator["drawers"]["top"][0] = "carrots"
+p refrigerator["drawers"]["top"]
