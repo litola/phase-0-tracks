4897af2caa73c6ca626f1d481242b8cad1b10934
Rafael Lara <ing.lara@live.com>
2017-02-24 16:06:32 -0600

    Update files and create db

--- databases/solo_project/admin_sys.rb
+++ databases/solo_project/admin_sys.rb
@@ -1,4 +1,4 @@
 #require sqlite 3 gem
 require 'sqlite3'
 #create a administration system db
-
+db = SQLite3::Database.new "admin_sys.db"
--- databases/solo_project/payroll.rb
+++ databases/solo_project/payroll.rb
@@ -1,3 +1,2 @@
 # create payroll table
 # => retrieve assistance by workers and multiply by daily salary
-# => add/update workers assistance
