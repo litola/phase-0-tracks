45041cd5873e8e2c1eda0432900158d4b94c49f5
Rafael Lara <ing.lara@live.com>
2017-02-24 17:39:32 -0600

    Add namespace to reporter

--- databases/solo_project/reporter.rb
+++ databases/solo_project/reporter.rb
@@ -1,10 +1,13 @@
 module Reporter
 #retrieve all companies
-	def self.all_companies(db)
-		companies = db.execute("SELECT * FROM companies")
-		puts "--    Companies Directory    --"
-		companies.each do |c|
-			puts "#{c['id']}. Name: #{c['name']} Phone: #{c['phone']}"
+	class Company
+		def self.all_companies(db)
+			companies = db.execute("SELECT * FROM companies")
+			puts "--    Companies Directory    --"
+			companies.each do |c|
+				puts "#{c['id']}. Name: #{c['name']} Phone: #{c['phone']}"
+			end
 		end
+		
 	end
 end
