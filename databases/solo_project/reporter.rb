619566f3a2b183fd86a4196ead573797c82217cd
Rafael Lara <ing.lara@live.com>
2017-02-24 19:53:11 -0600

    Add workers from company in reporter

--- databases/solo_project/reporter.rb
+++ databases/solo_project/reporter.rb
@@ -8,7 +8,8 @@ module Reporter
 			companies.each do |c|
 				puts "#{c['id']}. Name: #{c['name']} Phone: #{c['phone']}"
 			end
-		end
+			puts
+		end 
 	end
 
 	class Worker
@@ -17,8 +18,19 @@ module Reporter
 			workers = db.execute("SELECT * FROM workers")
 			puts "--    Workers Directory    --"
 			workers.each do |worker|
+				puts "#{worker['id']}. Name: #{worker['name']} Phone: #{worker['phone']} Daily salary: $#{worker['daily_salary']} Company: #{worker['company_id']}"
+			end
+			puts
+		end
+		#all workers from one company
+		def self.workers_from_company(db,company_id)
+			company = db.execute("SELECT * FROM companies Where id = ?",[company_id])
+			workers = db.execute("SELECT * FROM workers Where company_id = ?",[company_id])
+			puts "--    Workers from #{company[0]['name'].upcase}:  --"
+			workers.each do |worker|
 				puts "#{worker['id']}. Name: #{worker['name']} Phone: #{worker['phone']} Daily salary: $#{worker['daily_salary']}"
 			end
+			puts
 		end
 	end
 
